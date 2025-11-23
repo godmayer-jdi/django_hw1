7d93c972698dec30a413c8f4a14f018115e386b1
godmayer-jdi <tig774ra@gmail.com>
2025-11-23 22:06:25 +0400

    В модуле Models.py создали Модели Category b Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+"""Создаём модель Category"""
+class Category(models.Model):
+    name = models.CharField("Наименование", max_length=255)
+    description = models.TextField("Описание")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+"""Создаём модель Product"""
+class Product(models.Model):
+    name = models.CharField("Наименование", max_length=255)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to='products/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    price = models.DecimalField("Цена за покупку", max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
