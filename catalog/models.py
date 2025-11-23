4bbb113908548227372f33da211b0cc6c17469a7
godmayer-jdi <tig774ra@gmail.com>
2025-11-24 01:12:58 +0400

    явное объявление менеджера объектов в модели

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 """Создаем модель Category"""
 class Category(models.Model):
+    objects = models.Manager()
     name = models.CharField("Наименование", max_length=255)
     description = models.TextField("Описание")
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,6 +16,7 @@ class Category(models.Model):
 
 """Создаем модель Product"""
 class Product(models.Model):
+    objects = models.Manager()
     name = models.CharField("Наименование", max_length=255)
     description = models.TextField("Описание")
     image = models.ImageField("Изображение", upload_to='products/')
