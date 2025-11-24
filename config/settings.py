9a8a4cd0fab13c7a03e8e6abfc9bfd69f7655f3d
godmayer-jdi <tig774ra@gmail.com>
2025-11-24 23:05:02 +0400

    внесены изменения по комментариям наставника: в setting.py удалён закомментированный код, переменные окружения переименованы в DB_NAME, DB_USER, DB_PORT и т.д. соответственно, аналогичное переименование выполнено в .env

--- config/settings.py
+++ config/settings.py
@@ -80,17 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'django_project',
-#        'USER': 'postgres',
-#        'PASSWORD': 'django_pass',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
