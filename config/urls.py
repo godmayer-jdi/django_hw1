f7a453bf00041315bcf82dc7c4b7970bdc1bdd7c
godmayer-jdi <tig774ra@gmail.com>
2025-12-01 01:11:52 +0400

    Регистрации маршрута подключения приложения

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "", include("catalog.urls")
-    ),  # подключаем маршруты приложения catalog по корню сайта
+    path("", include("catalog.urls")),  # подключаем маршруты приложения catalog по корню сайта
+    path('blogs/', include('blog.urls')),  # подключаем маршруты приложения blog по корню сайта
 ]
