b8dfaa9f6fc25aed0c14da436c4d88e8a5e852a4
godmayer-jdi <tig774ra@gmail.com>
2025-11-30 23:28:01 +0400

    Доработана страница Контакты: добавлен модуль forms.py в шаблоне contacts.html добавлен код формы обратной связи контроллеры в views.py и пути в urls.py переписаны с FBV на CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,45 @@
-from django.shortcuts import get_object_or_404, render
+from django.views.generic import TemplateView
+
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 
-
-def home(request):
-    products = Product.objects.all()
-    return render(request, "catalog/home.html", {"products": products})
+from .forms import ContactForm
 
 
-def contacts(request):
-    return render(request, "catalog/contacts.html")
+class HomeView(TemplateView):
+    template_name = 'catalog/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, "catalog/product_detail.html", {"product": product})
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.method == 'POST':
+            context['form'] = ContactForm(self.request.POST)
+            if context['form'].is_valid():
+                context['form'].save()
+                context['success_message'] = 'Спасибо за сообщение!'
+                context['form'] = ContactForm()  # Очистка формы
+        else:
+            context['form'] = ContactForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)  # Обрабатываем POST как GET
+
+
+class ProductDetailView(TemplateView):
+    template_name = 'catalog/product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = kwargs.get('pk')
+        context['product'] = get_object_or_404(Product, pk=pk)
+        return context
