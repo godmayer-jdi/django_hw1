6f13e0e7ed765462f9a60a30c6a49e1963edd6c4
godmayer-jdi <tig774ra@gmail.com>
2025-12-01 01:10:17 +0400

    Реализация CRUD для новой модели с использованием CBV. Создание модуля blog/urls.py для регистрации маршрутов к созданным CRUD представлениям

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import BlogPost
 
-# Create your views here.
+class BlogListView(ListView):
+    model = BlogPost
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_queryset(self):
+        return BlogPost.objects.all()
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/blog_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        return obj
+
+class BlogCreateView(CreateView):
+    model = BlogPost
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/blog_form.html'
+    success_url = reverse_lazy('blog_list')
+
+class BlogUpdateView(UpdateView):
+    model = BlogPost
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/blog_form.html'
+    success_url = '/blogs/{id}/'  # Перенаправление на страницу статьи
+
+    def get_success_url(self):
+        success_url = reverse_lazy('blog:blog_list')
+
+class BlogDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/blog_confirm_delete.html'
+    success_url = reverse_lazy('blog_list')
