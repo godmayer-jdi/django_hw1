77768b92a5e6df727eb42e8e4044d637268e4f73
godmayer-jdi <tig774ra@gmail.com>
2025-12-05 00:04:51 +0400

    Модуль urls.py дополнен маршрутами для CRUD

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,11 +7,11 @@ from . import views
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("contacts/", views.ContactsView.as_view(), name="contacts"),
-    path(
-        "product_detail/<int:pk>/",
-        views.ProductDetailView.as_view(),
-        name="product_detail",
-    ),
+    path("product_detail/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
+    #Новые маршруты СКГВ
+    path("product/create/", views.ProductCreateView.as_view(), name="product_create"),
+    path("product/<int:pk>/update/", views.ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete/", views.ProductDeleteView.as_view(), name="product_delete"),
 ]
 
 if settings.DEBUG:
