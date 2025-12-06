ec81e6c6df7a111ba7b6450ec77bc55aa322d74f
godmayer-jdi <tig774ra@gmail.com>
2025-12-06 20:09:04 +0400

    Создан модуль urls.py для приложения users. Прописаны пути для регистрации и входа в приложение. Прописан путь к приложению users в config/urls.py Обновлено стандартное содержимое apps.py

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "", include("catalog.urls")
-    ),  # подключаем маршруты приложения catalog по корню сайта
-    path(
-        "blogs/", include("blog.urls")
-    ),  # подключаем маршруты приложения blog по корню сайта
+    path("", include("catalog.urls")),  # подключаем маршруты приложения catalog по корню сайта
+    path("blogs/", include("blog.urls")),  # подключаем маршруты приложения blog по корню сайта
+    path("users/", include("users.urls", namespace='users')),  # подключаем маршруты приложения users по корню сайта
 ]
